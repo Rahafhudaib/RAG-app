fac1db02c2f4d551349e056d52a64489c2237797
Rahaf <rahafhudaib189@gmail.com>
2026-08-27 17:25:12 +0300

    #	modified:   SRC/routes/base.py

--- SRC/routes/base.py
+++ SRC/routes/base.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI , APIRouter
-from SRC.helpers.config import get_settings
+from fastapi import FastAPI , APIRouter , Depends
+from SRC.helpers.config import get_settings ,settings
 base_router=APIRouter(prefix="/api_v_1", tags=["api_v_1"])
 
 
 @base_router.get("/")   ##اي ابليكيشن بالدنيا انتبهييي يكون برد عالديفلت راوت لاغراض الهيلث تشيك وغيرهم
-async def read_root():
-    app_settings = get_settings()
+async def read_root(app_settings: settings = Depends(get_settings())):
+    
     app_name=app_settings.APP_NAME
     app_version=app_settings.APP_VERSION
     return {"app_name": app_name, "app_version": app_version}
